3ada5c4f11d16f05100f4fb050823900a2f9cef8
manoj-mmw <manoj@makemywin.com>
2022-04-15 16:02:56 +0530

    sms sending

--- assets/js/success-popup.js
+++ assets/js/success-popup.js
@@ -2,6 +2,32 @@ jQuery(document).ready(function ($) {
   //open popup
   $(".cd-popup-trigger").on("click", function (event) {
     event.preventDefault();
+
+    $(".cd-popup").addClass("is-visible");
+
+	const userInputField = document.getElementById('phone_number');
+	const mobileNumber = userInputField.value;
+
+	if(mobileNumber.match(/^(\+\d{1,3}[- ]?)?\d{10}$/) && ! (mobileNumber.match(/0{5,}/)) ) {
+
+		const apiUrl = 'https://2929-3-111-140-16.ngrok.io/auth/v1/auth/optInForInvitation/';
+		const body = {
+			mobileNumber
+		}
+
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				userInputField.value = '';
+			}
+		};
+		xhttp.open("POST", apiUrl, true);
+		xhttp.setRequestHeader("Content-type", "application/json");
+		xhttp.send(JSON.stringify(body));
+	}
+  });
+  $(".cd-popup-trigger").on("click", function (event) {
+    event.preventDefault();
     $(".cd-popup").addClass("is-visible");
   });
 
